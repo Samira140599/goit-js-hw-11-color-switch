ceab3d398970448367f757da9981588829b9eaf9
Samira140599 <67050302+Samira140599@users.noreply.github.com>
2020-12-15 16:43:08 +0300

    7

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ const colors = [
   ];
   const startButton = document.getElementById('start');
   const stopButton = document.getElementById('stop');
-  
+
+  let timerId = null;
+
   startButton.addEventListener("click", () => {
     timerId = setInterval(() => {
-        document.body.style.backgroundColor = colors[randomIntegerFromInterval(0,5)];
+        document.body.style.backgroundColor = colors[randomIntegerFromInterval(0,5)]
+        startButton.disabled = true;
     }, 1000);
   });
+  stopButton.addEventListener("click", () => {
+    startButton.disabled = false;
+    clearInterval(timerId);
+  });
 
+  stopButton.addEventListener("click", () => {
+    clearInterval(timerId);
+  });
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);}
 
